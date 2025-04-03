bdc8e440469125dbe66df1582d4331c1bf83cc5c
vuduc0612 <ducvu8628@gmail.com>
2025-04-03 10:05:17 +0700

    update response api

--- src/main/java/com/food_delivery_app/food_delivery_back_end/exception/AccountNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.food_delivery_app.food_delivery_back_end.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class AccountNotFoundException extends ApiException {
-    public AccountNotFoundException(String message) {
-        super(message, HttpStatus.NOT_FOUND);
-    }
-}
--- src/main/java/com/food_delivery_app/food_delivery_back_end/exception/DuplicateResourceException.java
+++
@@ -1,7 +0,0 @@
-package com.food_delivery_app.food_delivery_back_end.exception;
-
-public class DuplicateResourceException extends RuntimeException {
-    public DuplicateResourceException(String message) {
-        super(message);
-    }
-}
